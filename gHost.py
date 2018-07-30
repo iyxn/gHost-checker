1c0b9ce31c1829f0242cb1e3145677990f3240e7
HardGhost Alkori <hardghostalkori@gmail.com>
2018-07-30 20:48:16 +0700

    Alpha Release
    Mass HTTP requests Mode Direct Requests/Proxy

--- gHost.py
+++ gHost.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python
 # gHost HTTP Request Checker by HardGhost
 # Tools untuk mengecek http header dan host checker, cocok digunakan untuk mengecek header
-# bug operator 
+# bug operator secara massal.
 #
 # Tools ini membutuhkan: 
-# - Python 2.7, Android: qpython (playstore) 
 # - module requests
 #
-# *Panduan Instalasi* #
+# *Panduan Instalasi* 
 # 
 # Komputer(linux)/termux:
-# ketik command : python
-#               : pip install python
-#               : python gHost.py
-#
-# Android (non termux)(Rekomendasi):
-# - download qpython di playstore
-# - buka qpython, pilih qpypi lalu pilih pip dan jalankan
-# - ketik command pip install requests lalu kembali ke menu
-# - buka editor, buka file gHost.py dan jalankan. 
+# ketik command : python2
+#               : pip2 install requests
+#               : python2 gHost.py
 #
 # Note:
-#   kalian harus paham kode HTTP status seperti 200,301,400,403 dll. 
+#   kalian harus paham kode HTTP status seperti 200,300,400,400,500 dll. 
 #
 # Credit: Hardian 'HardGhost' Alkori
 # Web: HardGhost Security | http://hardghost-sec.blogspot.com
@@ -29,9 +22,12 @@
 
 
 # mengimport module
+from datetime import datetime
 import requests 
 import os
 import time
+import sys
+import fileinput
 
 clear = os.system('clear') 
 clear
@@ -39,125 +35,108 @@ clear
 # opening
 print ' gHost HTTP Header Requests Checker' 
 print 'Bug Report hardghostalkori@gmail.com' 
-print '  | hardghost-sec.blogspot.com |'
+print ' |        ScarletDev Studio        |\n' 
+print 'Web: hardghost-sec.blogspot.com |'
+print 'Git: http://github.com/hardghost07'
 print
-print'Deskripsi Update Terbaru:\n'
-print'- Deteksi Otomatis Protokol HTTP/HTTPS'
-print'- Perbaikan Beberapa Bug\n' 
+print 'Note: Belum Support SSL, Tapi Coba Aja.\n'
 
-# membuat blok statemen 
-def urlcheck() :
-# handling exceptions
- try:
-     url = raw_input('Url (Ctrl+c Untuk Keluar):') 
-     print    	
-     print 'Checking HTTPS Protocol....\n' 
-     rq = requests.get('https://'+url,timeout=5, allow_redirects=False)
- except requests.exceptions.ConnectionError:
-     print
-     print'Invalid Hostname\n'
-     print 'But Don/t Worry, I will check\nHTTP Protocol...\n' 
-     try:
-         rq = requests.get('http://'+url,timeout=5, allow_redirects=False)
-         print '[!] HTTP IS ALIVE [!]\n'
-     except requests.exceptions.ConnectionError:
-         print'Timed Out, Connection reset by peer \n'
-         while True:
-          urlcheck()
-     except requests.exceptions.ReadTimeout:
-          print'Timed Out, Connection reset by peer \n'
-          while True:
-              urlcheck() 
- except requests.exceptions.InvalidURL:
-     print
-     print 'Invalid URL\n' 
-     while True:
-         urlcheck()
- except requests.exceptions.ReadTimeout:
-     print 'request timed out, but don`t worry,\ni will check http protocol... \n'
-     try:
-         rq = requests.get('http://'+url,timeout=5, allow_redirects=False)
-         print' - - - - - - - - - - - - - - - - - --'
-         print'[!] HTTP Is Alive [!]\n' 
-     except requests.exceptions.ReadTimeout:
-         print'Timeout, connection reset by peer (bug mati)\n'
-         while True:
-             urlcheck() 
- except KeyboardInterrupt:
-     print 'Bye Bye....' 
-# Menampilkan Request 
- print'Request Address:',rq.url
- print'Encoding:', rq.encoding
- try:
-     headers1 = rq.headers ['Content-Type']
-     headers2 = rq.headers ['Connection']
-     headers3 = rq.headers ['Date'] 
- except KeyError:
-     print 'Content-Type: -'
-     print 'Connection: -' 
-     print 'Date: -' 
- else:
-     print 'Content-Type:', headers1
-     print 'Connection:', headers2
-     print 'Date:', headers3
- raw = rq.raw.version
- if raw == 11:
-    print 'HTTP Version: HTTP/1.1'
- else:
-    print 'HTTP Version: HTTP/1.0'
- stat1 = rq.status_code
- if stat1 == 200:
-     print "Status: 200" 
-     print "----------------" 
-     print
-     while True:
-         urlcheck()
- else:
-     print 'Status:', rq.status_code
- try:
-     loc = rq.headers['Location']
-     
- except KeyError:   
-     print 'Redirect To: -\n' 
-     urlcheck() 
- else:
-     print 'Redirect To: ', loc
- print
+try:
+   filename = raw_input("Path File Bug Host(txt): ")
+   if os.path.exists(filename) == False:
+             print "File Not Found!" 
+             exit()
+   print
+   print 'Contoh Proxy:' 
+   print '-squid.jagoanssh.com:80'
+   print '-proxy.configinternet.host:80\n'
+   print "Enter/Lewati Untuk Mode Direct"
+   proxy_http = raw_input("HTTP Proxy Host:Port : ")
+   proxydict = {
+   	             "http" : proxy_http
+   	            }          
+   output = raw_input('Simpan File Hasil Dengan Nama? (*.txt): ')
+   print
 
-# menampilkan url yang valid jika status url pertama terjadi redirect url(301,302)
- print 'Checking Redirected Address....\n'
- try:
-    req2 = requests.get(loc, timeout = 5, allow_redirects=True)
- except requests.exceptions.ConnectTimeout:
-    print 'HTTP/HTTPS Timed Out'
-    urlcheck()
- except requests.exceptions.ReadTimeout:
-    print 'HTTP/HTTPS Timed Out' 
-    print '--------------------\n' 
-    urlcheck() 
- print '---------------------------------------' 
- print 'Valid address:', req2.url
- print 'Status: ', req2.status_code
- print 'Encoding:', rq.encoding
- raw = rq.raw.version
- if raw == 11:
-    print 'HTTP Version: HTTP/1.1'
- else:
-    print 'HTTP Version: HTTP/1.0'
- try:
-     headers1 = rq.headers ['Content-Type']
-     headers2 = rq.headers ['Connection']
-     headers3 = rq.headers ['Date'] 
- except KeyError:
-     print 'Content-Type: -'
-     print 'Connection: -' 
-     print 'Date: -' 
- else:
-     print 'Content-Type:', headers1
-     print 'Connection:', headers2
-     print 'Date:', headers3
- print '---------------------------------------\n'
-urlcheck() 
+except KeyboardInterrupt:
+    print "Aborted"
 
-while True:
-    urlcheck() 
+os.system('clear')
+wk = str(datetime.now())
+print 'Checker Started At'
+print wk
+print
+
+def exe():
+ with open(filename) as fp: 
+    for line in fp:        
+        line = line.rstrip()
+        try:
+            if proxy_http == '':
+                print 'Host:', line,'- Direct'
+            else:
+                print 'Host:', line,'-',proxy_http
+            r = requests.get("http://"+line,proxies = proxydict,allow_redirects=True,timeout = 5)              
+        except requests.exceptions.ReadTimeout:           
+            if proxy_http == '':
+                print 'Status: timeout'
+                print
+            else:
+                print "Status: Invalid Proxy or Proxy Timeout\n"
+        except requests.exceptions.TooManyRedirects:
+            print "Status: 302 Found"
+            print
+        except requests.exceptions.InvalidURL:
+            print "Invalid URL"
+            print
+        except requests.exceptions.ConnectTimeout:
+            if proxy_http == '':
+                print 'Status: timeout'
+                print
+            else:
+                print "Status: Invalid Proxy or Proxy Timeout\n"
+                print
+            print
+        except requests.exceptions.ConnectionError:
+            if proxy_http == '':
+                print 'Status: timeout'
+                print
+            else:
+                print "Status: Invalid Proxy or Proxy Timeout\n"
+                print
+            print
+        except requests.exceptions.ProxyError:
+            print 'Status: timeout'
+            print
+        else:
+            print "Status: ",r.status_code            
+            stat = r.status_code            	                    	                 	
+            if stat > 199:
+                f = open(output,"a")
+                if proxy_http == '':
+                 f.write(str(line+' : '))
+                else:
+                 f.write(str(line+'-'+proxy_http+' : '))
+                f.write('%s'%(stat)+'\n')
+            try:  
+             con = r.headers['Connection']
+            except KeyError:
+             print "Connection:-\n"
+            else:
+             print "Connection: ",con
+             print           
+exe()
+
+print '-- DONE --'
+print 'Checker Stopped At'
+print wk,'\n'
+
+if os.path.exists(output) == True:
+    with open(output) as out:
+        hitung_baris = 0
+        for line in out:
+            hitung_baris += 1
+        print '[!] DITEMUKAN', hitung_baris, 'HOST HIDUP [!] '
+        print 'Host Hidup Disimpan Dengan Nama', output, '\n'
+else:
+    print '[!] Maaf, Host Hidup Tidak Ditemukan gHost Checker [!]\n'
